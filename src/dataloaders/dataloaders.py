6cf522c8dfd0993d2400aa47b904193eafbbbc37
Diego Valenzuela <diegovalenzuela@protonmail.com>
2019-08-26 02:27:50 -0400

    updated dataloaders, Added HilbertDataLoader and build_dataloader_from_disk methods

--- src/dataloaders/dataloaders.py
+++ src/dataloaders/dataloaders.py
@@ -1,30 +1,34 @@
+from typing import Tuple
+
 import torch
 from torch.utils.data import DataLoader, random_split
 
-from src.dataloaders.datasets import LanguageModelDataset
+from src.dataloaders.datasets import LanguageModelDataset, HilbertDataset
 from src.dataloaders.PadCollate import PadCollate
 
 
 class LanguageModelDataLoader():
     def __init__(
             self,
-            filename: str = '/text/data/raw/horoscopo_raw.txt',
-            separator: str = ' ',
-            start_token: str = '<SOS>',
-            end_token: str = '<EOS>',
+            filename: str = "/text/data/raw/horoscopo_raw.txt",
+            separator: str = " ",
+            max_examples: int = -1,
+            #start_token: str = '<SOS>',
+            #end_token: str = '<EOS>',
             batch_size: int = 32,
-            num_workers=torch.get_num_threads(),  # TODO : add type hint
+            num_workers: int = torch.get_num_threads(),
             shuffle: bool = True,
             drop_last: bool = True,
             batch_first: bool = False,
             padding_value: int = -1,
-            dev_spli: float = 0.1) -> None:
+            dev_split: float = 0.1) -> None:
 
         # Language Model Dataset
         self.filename = filename
         self.separator = separator
-        self.start_token = start_token
-        self.end_token = end_token
+        self.max_examples = max_examples
+        #self.start_token = start_token
+        #self.end_token = end_token
 
         # Language Model Data Loader
         self.batch_size = batch_size
@@ -39,16 +43,20 @@ class LanguageModelDataLoader():
         # Random Split
         self.dev_split = dev_split
 
-    def __dataset__(self):  # TODO : Add type hint
+    def __dataset__(self) -> LanguageModelDataset:
 
         # Dataset
-        dataset = LanguageModelDataset(filename=self.filename,
-                                       separator=self.separator,
-                                       start_token=self.start_token,
-                                       end_token=self.end_token)
+        dataset = LanguageModelDataset(
+            filename=self.filename,
+            separator=self.separator,
+            max_examples=self.max_examples
+            #start_token=self.start_token,
+            #end_token=self.end_token
+        )
+
         return dataset
 
-    def data_loader(self):  # TODO : Add type hint
+    def data_loader(self) -> Tuple[DataLoader, DataLoader]:
 
         dataset = self.__dataset__()
 
@@ -86,3 +94,21 @@ class LanguageModelDataLoader():
 
     def __len__(self) -> int:
         return len(self.__dataset__())
+
+
+def HilbertDataLoader(filename: str):
+    return HilbertDataset(filename=filename)
+
+
+def build_dataloader_from_disk(filename: str,
+                               minibatch_size: int,
+                               shuffle: bool = True
+                               ) -> torch.utils.data.DataLoader:
+
+    dataloader = torch.utils.data.DataLoader(
+        HilbertDataLoader(filename=filename),
+        batch_size=minibatch_size,
+        shuffle=shuffle,
+    )
+
+    return dataloader
