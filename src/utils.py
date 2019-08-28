23973d8a8f341d6977f9d30afe7e8a60b298fb73
Diego Valenzuela <diegovalenzuela@protonmail.com>
2019-08-28 03:05:09 -0400

    Updated train.py with train_step method, and some refactor
    Fixed import issues. Use a generic device if no one is passed. Defined some dictionaries/JSONs for configurations.

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,64 @@ import os
 import torch
 
 
+def get_args():
+
+    parser = argparse.ArgumentParser('Train Hilbert AutoEncoder')
+
+    parser.add_argument('--device',
+                        type=str,
+                        default=None,
+                        help="torch device as a string")
+
+    parser.add_argument('--hdf5_file', type=str, help='Path to HDF5 file')
+
+    parser.add_argument('--checkpoint',
+                        type=str,
+                        default=None,
+                        help='Path to Checkpoint Model')
+
+    parser.add_argument('--epochs',
+                        type=int,
+                        default=100,
+                        help='Number of epochs')
+
+    parser.add_argument('--early_stop',
+                        type=int,
+                        default=40,
+                        help='Early stop limit')
+
+    parser.add_argument('--lr',
+                        type=float,
+                        default=0.001,
+                        help='learning rate')
+
+    parser.add_argument('--weight_decay',
+                        type=float,
+                        default=1e-5,
+                        help='weight decay to optimizer')
+
+    parser.add_argument('--batch_size',
+                        type=int,
+                        default=256,
+                        help='Batch size')
+
+    parser.add_argument('--nc',
+                        type=int,
+                        default=1,
+                        help='Number of channels in data')
+
+    parser.add_argument('--ld',
+                        type=int,
+                        default=256,
+                        help='latent dimension size')
+
+    args, _ = parser.parse_known_args()
+
+    args = parser.parse_args()
+
+    return args
+
+
 def load_from_checkpoint(model: torch.nn.Module, path_to_checkpoint: str):
     """
     Load from checkpoint.
@@ -53,45 +111,3 @@ def create_folders(path: str = "./output/") -> None:
         os.mkdir(path)
 
     return
-
-
-def get_args():
-    parser = argparse.ArgumentParser('Train Hilbert AutoEncoder')
-    parser.add_argument('--hdf5_file', type=str, help='Path to HDF5 file')
-    parser.add_argument('--checkpoint',
-                        type=str,
-                        default=None,
-                        help='Path to Checkpoint Model')
-    parser.add_argument('--epochs',
-                        type=int,
-                        default=100,
-                        help='Number of epochs')
-    parser.add_argument('--early_stop',
-                        type=int,
-                        default=40,
-                        help='Early stop limit')
-    parser.add_argument('--lr',
-                        type=float,
-                        default=0.001,
-                        help='learning rate')
-    parser.add_argument('--weight_decay',
-                        type=float,
-                        default=1e-5,
-                        help='weight decay to optimizer')
-    parser.add_argument('--batch_size',
-                        type=int,
-                        default=256,
-                        help='Batch size')
-    parser.add_argument('--nc',
-                        type=int,
-                        default=1,
-                        help='Number of channels in data')
-    parser.add_argument('--ld',
-                        type=int,
-                        default=256,
-                        help='latent dimension size')
-
-    args, _ = parser.parse_known_args()
-    args = parser.parse_args()
-
-    return args
