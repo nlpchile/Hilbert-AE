b884709d826dffd00d11822d0641525d64e60533
Diego Valenzuela <diegovalenzuela@protonmail.com>
2019-08-27 22:57:51 -0400

    Added some TODOs  to make it more general
    updated documentation and type annotations, deleted useless comments, renamed some arguments

--- src/dataloaders/datasets.py
+++ src/dataloaders/datasets.py
@@ -1,4 +1,6 @@
-from typing import Callable, List, Tuple
+"""This module implements the Datasets classes and methods."""
+
+from typing import Callable, List, Tuple, Optional
 
 import h5py
 import numpy as np
@@ -6,47 +8,47 @@ import torch
 from torch.utils.data import Dataset
 
 from src.dataloaders.Tokens import Tokens
-from src.utils import HilbertMapper, hilbert_curve, sequence2hilbert
 
 
 class LanguageModelDataset(Dataset):
+    """Language Model Dataset."""
 
-    # TODO: Perhaps we could add a stride parameter and see what happens
-    # with its latent space representation if we train a model to predict
-    # the next character given a context, for example, when using teacher
-    # forcing.
+    def __init__(self,
+                 filename: str,
+                 separator: str,
+                 max_number_of_examples: int = -1) -> None:
+        """
+        Language Model Dataset.
 
-    def __init__(
-            self,
-            filename: str,
-            separator: str,
-            max_examples: int = -1,
-            #start_token: str = '<SOS>',
-            #end_token: str = '<EOS>',
-    ) -> None:
+        Args:
+            filename (str) : Path to the raw text.
+
+            separator (str) : A string identifier used to split the string
+                              into its respective tokens.
+
+            max_number_of_examples (int) : Max number of examples to load. To
+                                           load them all use
+                                           max_number_of_examples=-1 .
+
+        """
         super(LanguageModelDataset, self).__init__()
 
         self.filename = filename
         self.separator = separator
-        self.max_examples = max_examples
-        #self.start_token = start_token
-        #self.end_token = end_token
+        self.max_number_of_examples = max_number_of_examples
 
         # Dictionaries
         self.tokens = Tokens()
-        #self.tokens.add_token(self.start_token)  # start of sentence
-        #self.tokens.add_token(self.end_token)  # end of sentence
 
         self.lines, self.out, self.lengths = self.__load__()
 
+        # TODO: Perhaps we could add a stride parameter and see what happens
+        # with its latent space representation if we train a model to predict
+        # the next character given a context, for example, when using teacher
+        # forcing.
+
     def __load__(self) -> Tuple[List[List[str]], List[List[int]], List[int]]:
-        '''
-
-        It returns a list of tokenized strings, the list of its
-        respectives tokens, and the lengths of each one.
-
-        '''
-
+        """Return the tokenized strings, its respectives tokens and lengths."""
         lines = []
         out, lengths = [], []
 
@@ -65,26 +67,35 @@ class LanguageModelDataset(Dataset):
                 for element in elements:
                     self.tokens.add_token(element)
 
-                if len(lines) > self.max_examples and self.max_examples != -1:
+                if len(
+                        lines
+                ) > self.max_number_of_examples and self.max_number_of_examples != -1:
                     break
 
             # After the words has been added to the dictionary
             for line in lines:
-                idxs = [self.tokens.token2index[token] for token in line]
-                length = len(idxs)
+                indexes = [self.tokens.token2index[token] for token in line]
+                length = len(indexes)
                 lengths.append(length)
-                out.append(idxs)
+                out.append(indexes)
 
         return lines, out, lengths
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self, index: int):
+        """
+        Get one item at a time.
 
-        x = self.lines[idx]
+        Args:
+            index (int) : An integer containing the index of the item to
+                          retrieve.
+
+        Returns:
+            (torch.Tensor) :  It returns an item as a torch.Tensor.
+
+        """
+        x = self.lines[index]
 
         sequence_list = []
-
-        #embedding = np.array(self.tokens.token2index[self.start_token])
-        #sequence_list.append(torch.from_numpy(embedding))
 
         for token in x:
 
@@ -96,39 +107,49 @@ class LanguageModelDataset(Dataset):
 
             sequence_list.append(torch.from_numpy(embedding))
 
-        #embedding = np.array(self.tokens.token2index[self.end_token])
-        #sequence_list.append(torch.from_numpy(embedding))
-
-        # Sentence :
-        #                 "hola como estás ?"
-        # Then,
-        #         x = [ '<SOS>' , 'hola' , 'cómo' , 'estás' , '?' ]
-        #         y = [ 'hola' , 'cómo' , 'estás' , '?' , '<EOS>' ]
-
         x = torch.stack(sequence_list)
 
         return x
 
     def __len__(self) -> int:
-
+        """Return the length of the dataset."""
         return len(self.lines)
 
 
 class HilbertDataset(torch.utils.data.Dataset):
+    """Hilbert Dataset."""
+
     def __init__(self, filename: str) -> None:
+        """
+        Hilbert Dataset.
+
+        Args:
+            filename (str) : A string containing the filename to the dataset.
+
+        """
         super(HilbertDataset, self).__init__()
 
-        # TODO : We could rename it and make it a more general
-        # dataloader that reads from binary files
+        # TODO : We could rename this class and make it a more general dataloader that reads from binary files
 
         # we could use a list of keys
         self.key_name = "hilbert"
 
         self.h5pyfile = h5py.File(filename, "r")
-        self.num_seq = self.h5pyfile[self.key_name].shape[0]
+        self.number_of_sequences = self.h5pyfile[self.key_name].shape[0]
 
     def __getitem__(self, index: int) -> torch.Tensor:
+        """
 
+        Get one item at a time.
+
+        Args:
+            index (int) : An integer containing the index of the item to
+                          retrieve.
+
+        Returns:
+            (torch.Tensor) : It returns an item as a torch.Tensor.
+
+        """
         mapped_sequence = torch.Tensor(
             self.h5pyfile[self.key_name][index, :, :, :]).type(
                 dtype=torch.long)
@@ -136,13 +157,42 @@ class HilbertDataset(torch.utils.data.Dataset):
         return mapped_sequence
 
     def __len__(self) -> int:
-        return self.num_seq
+        """Return the length of the dataset."""
+        return self.number_of_sequences
 
 
-def process_file(dataset: Dataset, mapper: Callable, output_file: str,
-                 order: int, vocabulary_size: int) -> None:
+def process_file(dataset: Dataset,
+                 mapper: Callable,
+                 output_file: str,
+                 order: int,
+                 vocabulary_size: int,
+                 name: str = "hilbert",
+                 dtype: Optional[str, type] = "int32") -> None:
+    """
+    Create a binary file from a given dataset.
 
-    # TODO : Test process_file method
+    Args:
+        dataset (Dataset): A torch Dataset object.
+
+        output_file (str): A string with the output file name.
+
+        name (str) : Name of the dataset.
+
+        dtype (Optional[int, type]) : Numpy dtype or string. It overrides the
+                                      data dtype.
+
+        mapper (Callable): A callable that recieves an input item from the
+                           Dataset and outputs its mapped representation.
+
+        order (int): An integer with the Hilbert curve order.
+
+        vocabulary_size (int): Vocabulary size.
+
+    """
+    # TODO: Perhaps we could rename this method and process the input earlier, perhaps using the Callable mapper as an argument for the Dataset Class, and then infer the data shape also from the Dataset.
+
+    # TODO: Perhaps we can get an item "shape" Tuple as input instead of an "order" and "vocabulary_size" argument.
+    shape: Tuple = (order, order, vocabulary_size)
 
     # create output file
     f = h5py.File(output_file, "w")
@@ -150,25 +200,24 @@ def process_file(dataset: Dataset, mapper: Callable, output_file: str,
     current_buffer_size = 1
     current_buffer_allocation = 0
 
-    # mapper.shape = tuple( [dim for dim in hilbert_map.shape] + [number_of_channels])
-
     dataset_HDF5 = f.create_dataset(
-        "hilbert", (current_buffer_size, order, order, vocabulary_size),
-        maxshape=(None, order, order, vocabulary_size),
-        dtype="int32")
+        name=name,
+        shape=(current_buffer_size, *shape),
+        dtype=dtype,
+        maxshape=(None, *shape),
+    )
 
-    for idx in range(0, len(dataset)):
+    for index in range(0, len(dataset)):
 
-        encoded_sequence = dataset[idx]
+        encoded_data = dataset[index]
 
-        mapped_sequence = mapper(encoded_sequence)
+        mapped_data = mapper(encoded_data)
 
         if current_buffer_allocation >= current_buffer_size:
             current_buffer_size = current_buffer_size + 1
-            dataset_HDF5.resize(
-                (current_buffer_size, order, order, vocabulary_size))
+            dataset_HDF5.resize((current_buffer_size, *shape))
 
-        dataset_HDF5[current_buffer_allocation] = mapped_sequence
+        dataset_HDF5[current_buffer_allocation] = mapped_data
 
         current_buffer_allocation += 1
 
