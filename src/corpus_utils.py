7a34e41dd56f69a3661d3429a135e68eb4f986d4
Diego Valenzuela <diegovalenzuela@protonmail.com>
2019-09-15 22:15:11 -0300

    yapf formatter

--- src/corpus_utils.py
+++ src/corpus_utils.py
@@ -5,13 +5,14 @@ import torch
 import numpy as np
 import h5py
 
+
 def remove_numbers(text):
-    text = re.sub(r'\d+', '', text)
+    text = re.sub(r"\d+", "", text)
     return text
 
 
 def remove_punctuation(text):
-    text = re.sub(r'[^\w\s]','',text)
+    text = re.sub(r"[^\w\s]", "", text)
     return text
 
 
@@ -19,6 +20,7 @@ def tokenizer_function(text):
     # text = clean_str(text)
     text = [x for x in text.split(" ") if x != "" and x.find(" ") == -1]
     return text
+
 
 def preprocess_text(text):
     text = text.replace("\\xa0", " ")
@@ -28,6 +30,7 @@ def preprocess_text(text):
     text = text.lower()
     return tokenizer_function(text)
 
+
 def get_sentences_in_batch(x, vocab):
     for sent in x:
         str1 = ""
@@ -36,12 +39,14 @@ def get_sentences_in_batch(x, vocab):
         print(str1)
 
 
-def helper_h5py(dset1,
-                dset2,
-                text_encodded,
-                current_buffer_allocation,
-                current_buffer_size,
-                max_sequence_length,):
+def helper_h5py(
+        dset1,
+        dset2,
+        text_encodded,
+        current_buffer_allocation,
+        current_buffer_size,
+        max_sequence_length,
+):
 
     if current_buffer_allocation >= current_buffer_size:
         current_buffer_size = current_buffer_size + 1
@@ -75,7 +80,7 @@ class Corpus(object):
         self.max_sequence_dataset = 0
 
     def add_word(self, word):
-        last_idx = len(self.word2idx) + 1 # TODO: parten desde 1
+        last_idx = len(self.word2idx) + 1  # TODO: parten desde 1
         if word not in self.word2idx:
             self.word2idx[word] = last_idx
             self.idx2word[last_idx] = word
@@ -110,64 +115,65 @@ class Corpus(object):
                 self.corpus.append(text)
                 if len(text) > self.max_sequence_dataset:
                     self.max_sequence_dataset = len(text)
-                
+
     def save_h5py(self, path_to_ofile, path_to_ifile=None):
-        f = h5py.File(path_to_ofile, 'w')
+        f = h5py.File(path_to_ofile, "w")
         current_buffer_size = 1
         current_buffer_allocation = 0
-        dset1 = f.create_dataset('sequence', 
-                             (current_buffer_size, self.max_sequence_length),
-                             maxshape=(None, self.max_sequence_length), 
-                             dtype='int32')
-        dset2 = f.create_dataset('mask', 
-                                (current_buffer_size, self.max_sequence_length), 
-                                maxshape=(None, self.max_sequence_length),
-                                dtype='uint8')
+        dset1 = f.create_dataset(
+            "sequence", (current_buffer_size, self.max_sequence_length),
+            maxshape=(None, self.max_sequence_length),
+            dtype="int32")
+        dset2 = f.create_dataset(
+            "mask", (current_buffer_size, self.max_sequence_length),
+            maxshape=(None, self.max_sequence_length),
+            dtype="uint8")
 
         if path_to_ifile == None:
             if len(self.corpus) == 0:
                 raise ValueError
-            else: 
+            else:
                 for text in self.corpus:
-                    dset1, dset2, current_buffer_allocation, current_buffer_size = helper_h5py(dset1,
-                                                                                               dset2,
-                                                                                               self.encode(text),
-                                                                                               current_buffer_allocation,
-                                                                                               current_buffer_size,
-                                                                                               self.max_sequence_length)
+                    dset1, dset2, current_buffer_allocation, current_buffer_size = helper_h5py(
+                        dset1, dset2, self.encode(text),
+                        current_buffer_allocation, current_buffer_size,
+                        self.max_sequence_length)
         else:
             with open(path_to_ifile, "r") as freader:
                 for line in freader:
                     text = preprocess_text(line)
-                    dset1, dset2, current_buffer_allocation, current_buffer_size = helper_h5py(dset1,
-                                                                                               dset2,
-                                                                                               self.encode(text),
-                                                                                               current_buffer_allocation,
-                                                                                               current_buffer_size,
-                                                                                               self.max_sequence_length)
+                    dset1, dset2, current_buffer_allocation, current_buffer_size = helper_h5py(
+                        dset1, dset2, self.encode(text),
+                        current_buffer_allocation, current_buffer_size,
+                        self.max_sequence_length)
 
     def save_corpus_parameters(self, outfile):
-        json_dict = {"max_sequence_dataset":self.max_sequence_dataset,
-                     "vocab_size":len(self.word2idx),
-                     "word2idx":self.word2idx,
-                     "idx2word":self.idx2word,
-                     "frequencies":self.frequencies
-                     }
+        json_dict = {
+            "max_sequence_dataset": self.max_sequence_dataset,
+            "vocab_size": len(self.word2idx),
+            "word2idx": self.word2idx,
+            "idx2word": self.idx2word,
+            "frequencies": self.frequencies
+        }
 
-        with open(outfile, 'w') as fp:
-            json.dump(json_dict, fp,  indent=4)
+        with open(outfile, "w") as fp:
+            json.dump(json_dict, fp, indent=4)
 
 
 class H5PytorchDataset(torch.utils.data.Dataset):
     def __init__(self, filename, keys_types):
         """
-        filename (string): path to file with dataset in format h5py
-        keys_types (dictionary): dictionary with dataset name and data type.
-                                 example: {"sequence":torch.long, "mask":torch.uint8}
+
+        Args:
+
+            filename (string): path to file with dataset in format h5py
+            keys_types (dictionary): dictionary with dataset name and data type.
+                                    example: {"sequence":torch.long, "mask":torch.uint8}
+
         """
         super(H5PytorchDataset, self).__init__()
 
-        self.h5pyfile   = h5py.File(filename, 'r')
+        self.h5pyfile = h5py.File(filename, "r")
         self.keys_types = keys_types
         key0 = list(self.keys_types)[0]
         self.num_sequences = self.h5pyfile[key0].shape[0]
@@ -175,7 +181,8 @@ class H5PytorchDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         items = []
         for key, dtype in self.keys_types.items():
-            items.append(torch.Tensor(self.h5pyfile[key][index,:]).type(dtype=dtype))
+            items.append(
+                torch.Tensor(self.h5pyfile[key][index, :]).type(dtype=dtype))
         return items
 
     def __len__(self):
