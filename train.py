ee60b8b8f3940b4255843fa5c334447c85b44be3
Diego Valenzuela <diegovalenzuela@protonmail.com>
2019-09-19 23:58:28 -0300

    deleted unused code

--- train.py
+++ train.py
@@ -11,16 +11,25 @@ from src.dataloaders.dataloaders import build_dataloader_from_disk
 from src.utils import (create_folders, get_kwargs, load_from_checkpoint,
                        process_batch, set_config)
 
-# Just run
+# Install requirements :
+#           pip3 install -r requirements.txt
+
+# Install library in editable mode:
+#           python3 setup.py develop
+
+# Run this script
 #           python3 train.py --config-file="./config.json"
 
 
-def train(kwargs):
+def train(kwargs: Dict) -> None:
     """Train a Hilbert Autoencoder."""
 
     # Create tensorboard log folder if it doesn't exist
     path_to_tensorboard_logs = create_folders(
         path=kwargs["path_to_tensorboard_logs"], parents=True, exist_ok=True)
+
+    # TODO : Replace this with a logger
+    print("tensorboard --logdir={}".format(path_to_tensorboard_logs))
 
     # Tensorboard writer
     writer = SummaryWriter(log_dir=path_to_tensorboard_logs)
@@ -34,23 +43,6 @@ def train(kwargs):
     # Initialize Model
     # TODO : Define a get_models() method that outputs a dictionary
     model = autoencoder(**kwargs["autoencoder"]).to(device)
-
-    # TODO : Add this in config.
-    #
-    # checkpoint_paths = {
-    #     "model": path_to_model_checkpoint,
-    #     "optimizer": path_to_optimizer_checkpoint
-    # }
-    #
-    # TODO : Replace with this.
-    # checkpoint = load_from_checkpoint(model=model,
-    #                                   optimizer=optimizer,
-    #                                   device=device,
-    #                                   paths=paths)
-
-    path_to_checkpoint = Path(kwargs["path_to_checkpoints"])
-    if path_to_checkpoint is not None and path_to_checkpoint.exists():
-        model.load_state_dict(torch.load(str(path_to_checkpoint)))
 
     # TODO : Define a get_criterions() method that outputs a dictionary
     criterion = torch.nn.MSELoss(**kwargs["loss"])
@@ -67,26 +59,16 @@ def train(kwargs):
 
     epochs = kwargs["epochs"]
 
-    early_stop_limit = kwargs["early_stop"]
-
-    early_stop_count = 0
-
-    train_loss = []
-
-    create_folders()
-
-    # TODO : Add this to config json
-    best_path = "./output/HILBERT_AE_best.pth"
-
     for epoch in range(epochs):
 
-        loss_train = 0
+        # We could save 1 batch specific batch in order to watch
+        # the evolution of its latent representation over the
+        # training epochs.
 
         for idx, batch in enumerate(train_loader):
 
             batch = batch.to(device)
 
-            # TODO : We must update this method if we update the dataloader outputs.
             batch = process_batch(batch=batch)
 
             x = batch
@@ -98,31 +80,13 @@ def train(kwargs):
 
             loss, output, latent = output["loss"], output["x_hat"], output["z"]
 
-            # TODO : Add a tensorboard logger
-
-            loss_train += loss
-
-        # ===================log========================
-        # TODO : Add a tensorboard logger.
-        print("epoch [{}/{}], loss:{:.4f}".format(epoch + 1, epochs,
-                                                  loss_train.item() / idx))
-        train_loss.append(loss_train.item() / idx)
-
-        # TODO : We must also save the optimizer states.
-        if epoch % 10 == 0:
-            torch.save(model.state_dict(),
-                       "./output/HILBERT_AE_{}.pth".format(epoch))
-        if len(train_loss) > 2 and train_loss[-1] == min(train_loss):
-            torch.save(model.state_dict(), best_path)
-            early_stop_count = 0
-        else:
-            early_stop_count += 1
-
-        if early_stop_count > early_stop_limit:
-            break
+            # Logging Loss
+            writer.add_scalar(tag="train/loss",
+                              scalar_value=loss,
+                              global_step=iter,
+                              walltime=None)
 
     writer.close()
-    print("AutoEncoder was trained !!")
 
 
 if __name__ == "__main__":
